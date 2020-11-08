55d230a223f7aabe4e70daabab7942a09d8b25fc
Boldeusz <mnpower@poczta.onet.pl>
2020-11-08 13:13:15 +0100

    Signed-off-by: Boldeusz <mnpower@poczta.onet.pl>

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("I am scared of aliens!!!");
+        }
+    }
+}
