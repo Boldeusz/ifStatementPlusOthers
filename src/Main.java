e0009b7b9b0de97ecc79b90cad52ebd4425ebbe0
Boldeusz <mnpower@poczta.onet.pl>
2020-11-08 13:57:21 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,31 @@ public class Main {
                 System.out.println("Either or both the conditions are true");
             }
 
+            int newValue = 50;
+            if (newValue == 50) {
+                System.out.println("This is true");
+            }
+            boolean isCar = false;
+            if (!isCar) {
+                System.out.println("This is not supposed to happen");
+            }
+
+            isCar = true;
+            boolean wasCar = isCar ? true : false;
+            if (wasCar) {
+                System.out.println("wasCar is true");
+            }
+            double myFirstValue = 20.00d;
+            double mySecondValue = 80.00d;
+            double myTotalValues = (myFirstValue + mySecondValue) * 100.00d;
+            System.out.println("MyTotalValues = " + myTotalValues);
+            double theRemainder = myTotalValues % 30.00d;
+            System.out.println("theRemainder = " + theRemainder);
+            boolean isNotRemainder = (theRemainder ==0 ) ? true : false;
+            System.out.println("isNoRemainder = " + isNotRemainder);
+            if (!isNotRemainder) {
+                System.out.println("Got some remainder");
+            }
         }
     }
 }
