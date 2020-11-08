705cdfa5f1691baea62b0a514a40d93326d111a9
Boldeusz <mnpower@poczta.onet.pl>
2020-11-08 13:21:56 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main {
         if (isAlien == false) {
             System.out.println("It is not an alien!");
             System.out.println("I am scared of aliens!!!");
+
+            int topScore = 80;
+            if (topScore < 100) {
+                System.out.println("You got the high score!!");
+            }
+            int secondTopScore = 50;
+            if (topScore > secondTopScore && topScore <100) {
+                System.out.println("Greater than second top score and less than 100 ");
+            }
+
         }
     }
 }
